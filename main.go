c1f647dfe96c48616fb32f9a47687804f80d4963
lazzy <565260908lazzysun@gmail.com>
2022-12-04 19:52:28 +0800

    集成gin

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	// 1.创建路由
+	r := gin.Default()
+	// 2.绑定路由规则，执行的函数
+	// gin.Context，封装了request和response
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello gin!")
+	})
+	// 3.监听端口，默认在8080
+	r.Run(":8080")
+}
