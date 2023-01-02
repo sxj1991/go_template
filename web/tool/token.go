74620a822e5584178821bf13dececda9673cded6
lazzy <565260908lazzysun@gmail.com>
2023-01-02 19:44:40 +0800

    aes加密用户信息 自定义token 用户认证功能

--- web/tool/token.go
+++ web/tool/token.go
@@ -1,16 +1,21 @@
 package tool
 
 import (
-	"errors"
-	"github.com/dgrijalva/jwt-go"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/google/uuid"
+	"go_template/setting"
 	"go_template/share"
-	"math/rand"
+	"io"
 	"time"
 )
 
 type Token struct {
 	Exp   int64  `json:"exp"`
 	Token string `json:"encoded"`
+	Id    string `json:"id"`
 }
 
 //密钥
@@ -19,43 +24,50 @@ var secret = []byte("read@book@isGood")
 func GenToken(userName string) Token {
 	token, _ := createToken(userName)
 
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		setting.Log.Error(err)
+	}
+
 	return Token{
 		Exp:   time.Now().Add(share.TokenExpireDuration).Unix(),
 		Token: token,
+		Id:    uuid.String(),
 	}
 }
 
 func UnToken(encoded string) (string, error) {
-	//需要用和加密时同样的方式转化成对应的字节数组
-	token, err := jwt.Parse(encoded, func(token *jwt.Token) (interface{}, error) {
-		return secret, nil
-	})
-
-	if err != nil {
-		return "", err
-	}
-
-	if token.Claims == nil {
-		return "", errors.New("token解析出错")
-	}
-
-	claims := token.Claims.(jwt.MapClaims)
-
-	if claims["userName"] == nil {
-		return "", errors.New("token解析出错")
-	}
-
-	return claims["userName"].(string), nil
+	return string(aesDecryptCFB([]byte(encoded), secret)), nil
 }
 
 func createToken(userName string) (string, error) {
-	rand.Seed(time.Now().UnixNano())
-	claims := jwt.MapClaims{
-		"userName": userName, //保存的信息
-		"id":       rand.Int(),
+	return base64.StdEncoding.EncodeToString(aesEncryptCFB([]byte(userName), secret)), nil
+}
+
+// =================== CFB ======================
+func aesEncryptCFB(origData []byte, key []byte) (encrypted []byte) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
 	}
-	// 使用指定的签名方法创建签名对象
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
-	// 使用指定的secret签名并获得完整的编码后的字符串token
-	return token.SignedString(secret)
+	encrypted = make([]byte, aes.BlockSize+len(origData))
+	iv := encrypted[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(encrypted[aes.BlockSize:], origData)
+	return encrypted
+}
+func aesDecryptCFB(encrypted []byte, key []byte) (decrypted []byte) {
+	block, _ := aes.NewCipher(key)
+	if len(encrypted) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := encrypted[:aes.BlockSize]
+	encrypted = encrypted[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(encrypted, encrypted)
+	return encrypted
 }
